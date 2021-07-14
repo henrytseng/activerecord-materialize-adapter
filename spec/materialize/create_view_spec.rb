297e7d764a15104fa9a306287a61b4c0a7ef20c4
Henry Tseng <info@heycanvas.com>
2021-07-14 17:37:01 -0400

    Simple test
    Signed-off-by: Henry Tseng <info@heycanvas.com>

--- spec/materialize/create_view_spec.rb
+++ spec/materialize/create_view_spec.rb
@@ -2,12 +2,15 @@ require 'spec_helper'
 
 describe "Create View" do
   it "should handle an available connection" do
-    res = ActiveRecord::Base.connection.execute <<-SQL.squish
+    ActiveRecord::Base.connection.execute <<-SQL.squish
       CREATE MATERIALIZED VIEW pseudo_source (key, value) AS
         VALUES ('a', 1), ('a', 2), ('a', 3), ('a', 4),
-        ('b', 5), ('c', 6), ('c', 7);
+        ('b', 5), ('c', 6), ('c', 7)
     SQL
 
+    res = ActiveRecord::Base.connection.execute <<-SQL.squish
+      SELECT * FROM pseudo_source
+    SQL
 
     binding.pry
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ require "rspec/expectations"
 require 'activerecord-materialize-adapter'
 require 'helpers/database_helper'
 
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
 RSpec.configure do |config|
   config.include DatabaseHelper
 
